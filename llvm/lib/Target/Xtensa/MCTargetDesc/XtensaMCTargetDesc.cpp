45076f0490158655f907870e385e27928f6bdd3e
Noam Raz <noamraz8@gmail.com>
2022-12-09 18:00:35 +0200

    Regiser xtensa register info

--- llvm/lib/Target/Xtensa/MCTargetDesc/XtensaMCTargetDesc.cpp
+++ llvm/lib/Target/Xtensa/MCTargetDesc/XtensaMCTargetDesc.cpp
@@ -2,10 +2,14 @@
 #include "TargetInfo/XtensaTargetInfo.h"
 #include "XtensaMCAsmInfo.h"
 #include "llvm/MC/MCAsmInfo.h"
+#include "llvm/MC/MCRegisterInfo.h"
 #include "llvm/MC/TargetRegistry.h"
 #include "llvm/Support/Compiler.h"
 
 using namespace llvm;
+
+#define GET_REGINFO_MC_DESC
+#include "XtensaGenRegisterInfo.inc"
 
 static MCAsmInfo *createXtensaMCAsmInfo(const MCRegisterInfo &MRI,
                                         const Triple &TT,
@@ -13,14 +17,21 @@ static MCAsmInfo *createXtensaMCAsmInfo(const MCRegisterInfo &MRI,
   return new XtensaMCAsmInfo();
 }
 
+static MCRegisterInfo *createXtensaMCRegisterInfo(const Triple &TT) {
+  MCRegisterInfo *X = new MCRegisterInfo();
+  InitXtensaMCRegisterInfo(X, Xtensa::A0);
+  return X;
+}
+
 extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeXtensaTargetMC() {
   Target &T = getTheXtensaTarget();
 
   RegisterMCAsmInfoFn X(T, createXtensaMCAsmInfo);
 
+  TargetRegistry::RegisterMCRegInfo(T, createXtensaMCRegisterInfo);
+
   // TODO: Register these:
   // * MCInstrInfo
-  // * MCRegisterInfo
   // * MCSubtargetInfo
   // * MCInstrAnalysis
   // * MCCodeEmitter
--- llvm/lib/Target/Xtensa/MCTargetDesc/XtensaMCTargetDesc.h
+++ llvm/lib/Target/Xtensa/MCTargetDesc/XtensaMCTargetDesc.h
@@ -1,4 +1,9 @@
 #ifndef LLVM_LIB_TARGET_XTENSA_XTENSAMCTARGETDESC_H
 #define LLVM_LIB_TARGET_XTENSA_XTENSAMCTARGETDESC_H
 
+// Defines symbolic names for Xtensa registers.  This defines a mapping from
+// register name to register number.
+#define GET_REGINFO_ENUM
+#include "XtensaGenRegisterInfo.inc"
+
 #endif
