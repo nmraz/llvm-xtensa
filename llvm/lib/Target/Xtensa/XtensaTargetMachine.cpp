e1a9a5d0fd9f5d0a1db0c1e542fcbb4aae6fa61e
Noam Raz <noamraz8@gmail.com>
2022-12-19 23:58:02 +0200

    Initialize GlobalISel passes for xtensa

--- llvm/lib/Target/Xtensa/XtensaTargetMachine.cpp
+++ llvm/lib/Target/Xtensa/XtensaTargetMachine.cpp
@@ -5,7 +5,9 @@
 #include "llvm/ADT/Triple.h"
 #include "llvm/Analysis/TargetTransformInfo.h"
 #include "llvm/CodeGen/TargetPassConfig.h"
+#include "llvm/InitializePasses.h"
 #include "llvm/MC/TargetRegistry.h"
+#include "llvm/PassRegistry.h"
 #include "llvm/Support/CodeGen.h"
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Target/TargetLoweringObjectFile.h"
@@ -16,6 +18,9 @@ using namespace llvm;
 
 extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeXtensaTarget() {
   RegisterTargetMachine<XtensaTargetMachine> Register(getTheXtensaTarget());
+
+  PassRegistry *PR = PassRegistry::getPassRegistry();
+  initializeGlobalISel(*PR);
 }
 
 XtensaTargetMachine::XtensaTargetMachine(const Target &T, const Triple &TT,
