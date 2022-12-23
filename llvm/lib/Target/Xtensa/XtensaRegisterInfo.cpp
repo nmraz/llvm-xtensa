d08c73d1c2f37062f9df13c13ede51c80e9a51e8
Noam Raz <noamraz8@gmail.com>
2022-12-24 00:26:11 +0200

    Create `XtensaRegisterInfo` stub

---
+++ llvm/lib/Target/Xtensa/XtensaRegisterInfo.cpp
@@ -0,0 +1,44 @@
+#include "XtensaRegisterInfo.h"
+#include "MCTargetDesc/XtensaMCTargetDesc.h"
+#include "XtensaFrameLowering.h"
+#include "llvm/ADT/BitVector.h"
+#include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/TargetSubtargetInfo.h"
+#include "llvm/MC/MCRegister.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "xtensa-reg-info"
+
+#define GET_REGINFO_TARGET_DESC
+#include "XtensaGenRegisterInfo.inc"
+
+XtensaRegisterInfo::XtensaRegisterInfo() : XtensaGenRegisterInfo(Xtensa::A0) {}
+
+const MCPhysReg *
+XtensaRegisterInfo::getCalleeSavedRegs(const MachineFunction *MF) const {
+  // TODO: define in XtensaCallingConv.td, relevant only for call0
+  return nullptr;
+}
+
+BitVector XtensaRegisterInfo::getReservedRegs(const MachineFunction &MF) const {
+  BitVector Result;
+
+  // Return address
+  Result.set(Xtensa::A0);
+  // Stack pointer
+  Result.set(Xtensa::A1);
+
+  return Result;
+}
+
+void XtensaRegisterInfo::eliminateFrameIndex(MachineBasicBlock::iterator MI,
+                                             int SPAdj, unsigned FIOperandNum,
+                                             RegScavenger *RS) const {
+  // TODO
+}
+
+Register XtensaRegisterInfo::getFrameRegister(const MachineFunction &MF) const {
+  // TODO: only call0 supports a dedicated frame pointer, a15?
+  return Xtensa::A1;
+}
---
+++ llvm/lib/Target/Xtensa/XtensaRegisterInfo.h
@@ -0,0 +1,32 @@
+#ifndef LLVM_LIB_TARGET_XTENSA_XTENSAREGISTERINFO_H
+#define LLVM_LIB_TARGET_XTENSA_XTENSAREGISTERINFO_H
+
+#include "Xtensa.h"
+#include "llvm/CodeGen/MachineBasicBlock.h"
+#include <cstdint>
+
+#define GET_REGINFO_HEADER
+#include "XtensaGenRegisterInfo.inc"
+
+namespace llvm {
+
+class TargetRegisterClass;
+
+class XtensaRegisterInfo : public XtensaGenRegisterInfo {
+public:
+  XtensaRegisterInfo();
+
+  const MCPhysReg *getCalleeSavedRegs(const MachineFunction *MF) const override;
+
+  BitVector getReservedRegs(const MachineFunction &MF) const override;
+
+  void eliminateFrameIndex(MachineBasicBlock::iterator MI, int SPAdj,
+                           unsigned FIOperandNum,
+                           RegScavenger *RS = nullptr) const override;
+
+  Register getFrameRegister(const MachineFunction &MF) const override;
+};
+
+} // end namespace llvm
+
+#endif // LLVM_LIB_TARGET_XTENSA_XTENSAREGISTERINFO_H
