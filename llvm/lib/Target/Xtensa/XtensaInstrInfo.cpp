5158245119b6bb3657ae6a2246f380a0470ed873
Noam Raz <noamraz8@gmail.com>
2022-12-26 22:11:42 +0200

    Correctly define xtensa call pseudos

--- llvm/lib/Target/Xtensa/XtensaInstrInfo.cpp
+++ llvm/lib/Target/Xtensa/XtensaInstrInfo.cpp
@@ -1,4 +1,5 @@
 #include "XtensaInstrInfo.h"
+#include "MCTargetDesc/XtensaMCTargetDesc.h"
 #include "XtensaFrameLowering.h"
 
 using namespace llvm;
@@ -6,4 +7,6 @@ using namespace llvm;
 #define GET_INSTRINFO_CTOR_DTOR
 #include "XtensaGenInstrInfo.inc"
 
-XtensaInstrInfo::XtensaInstrInfo(XtensaSubtarget &ST) : Subtarget(ST) {}
+XtensaInstrInfo::XtensaInstrInfo(XtensaSubtarget &ST)
+    : XtensaGenInstrInfo(Xtensa::ADJCALLSTACKDOWN, Xtensa::ADJCALLSTACKUP),
+      Subtarget(ST) {}
