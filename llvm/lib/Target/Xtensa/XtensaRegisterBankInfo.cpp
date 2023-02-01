30316b18691bb4dad9d523e8b735f747da0b7fe1
Noam Raz <noamraz8@gmail.com>
2023-02-01 22:22:14 +0200

    Fix xtensa register bank selection

--- llvm/lib/Target/Xtensa/XtensaRegisterBankInfo.cpp
+++ llvm/lib/Target/Xtensa/XtensaRegisterBankInfo.cpp
@@ -2,41 +2,20 @@
 #include "MCTargetDesc/XtensaMCTargetDesc.h"
 #include "XtensaRegisterInfo.h"
 #include "llvm/ADT/SmallVector.h"
+#include "llvm/CodeGen/MachineFunction.h"
 #include "llvm/CodeGen/MachineInstr.h"
 #include "llvm/CodeGen/MachineOperand.h"
+#include "llvm/CodeGen/MachineRegisterInfo.h"
 #include "llvm/CodeGen/RegisterBank.h"
 #include "llvm/CodeGen/RegisterBankInfo.h"
 #include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/LowLevelTypeImpl.h"
 #include <cassert>
 
 using namespace llvm;
 
 #define GET_TARGET_REGBANK_IMPL
 #include "XtensaGenRegisterBank.inc"
-
-namespace llvm {
-namespace Xtensa {
-
-RegisterBankInfo::PartialMapping PartMappings[] = {
-    {0, 32, GPRRegBank},
-};
-
-enum ValueMappingIdx {
-  InvalidIdx = 0,
-  GPRIdx = 1,
-};
-
-RegisterBankInfo::ValueMapping ValueMappings[] = {
-    // Invalid mapping
-    {nullptr, 0},
-    // Up to 3 operands in GPRs
-    {&PartMappings[0], 1},
-    {&PartMappings[0], 1},
-    {&PartMappings[0], 1},
-};
-
-} // namespace Xtensa
-} // namespace llvm
 
 XtensaRegisterBankInfo::XtensaRegisterBankInfo(const TargetRegisterInfo &TRI) {}
 
@@ -60,12 +39,20 @@ XtensaRegisterBankInfo::getInstrMapping(const MachineInstr &MI) const {
     return Mapping;
   }
 
-  int NumOperands = MI.getNumOperands();
-  assert(NumOperands <= 3 && "Too many operands in instruction!");
+  const MachineFunction &MF = *MI.getParent()->getParent();
+  const MachineRegisterInfo &MRI = MF.getRegInfo();
 
-  // Up to 3 operands in GPRs
-  RegisterBankInfo::ValueMapping *OperandsMapping =
-      &Xtensa::ValueMappings[Xtensa::GPRIdx];
-  return getInstructionMapping(DefaultMappingID, 1, OperandsMapping,
-                               NumOperands);
+  unsigned NumOperands = MI.getNumOperands();
+
+  SmallVector<const ValueMapping *, 4> ValMappings(NumOperands);
+  for (unsigned Idx = 0; Idx < NumOperands; Idx++) {
+    if (MI.getOperand(Idx).isReg()) {
+      LLT Ty = MRI.getType(MI.getOperand(Idx).getReg());
+      auto Size = Ty.getSizeInBits();
+      ValMappings[Idx] = &getValueMapping(0, Size, Xtensa::GPRRegBank);
+    }
+  }
+
+  return getInstructionMapping(DefaultMappingID, 1,
+                               getOperandsMapping(ValMappings), NumOperands);
 }
