e1bdb68644fd79a0f3c463f9bf0f9d7bc6cd9ac1
Noam Raz <noamraz8@gmail.com>
2023-03-23 20:34:23 +0200

    Add encoding tests for xtensa `sext` instruction

--- llvm/test/MC/Xtensa/arith_misc.s
+++ llvm/test/MC/Xtensa/arith_misc.s
@@ -4,3 +4,13 @@
 # CHECK-INST: abs a5, a6
 # CHECK: encoding: [0x60,0x51,0x60]
 abs a5, a6
+
+# Instruction format RRR
+# CHECK-INST: sext a2, a3, 7
+# CHECK: encoding: [0x00,0x23,0x23]
+sext a2, a3, 7
+
+# Instruction format RRR
+# CHECK-INST: sext a2, a3, 15
+# CHECK: encoding: [0x80,0x23,0x23]
+sext a2, a3, 15
