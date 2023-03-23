a171b1a962674901f835ac577fe50f49cc9f6f54
Noam Raz <noamraz8@gmail.com>
2023-03-23 20:10:55 +0200

    Add encoding tests for narrow loads and stores

--- llvm/test/MC/Xtensa/memory.s
+++ llvm/test/MC/Xtensa/memory.s
@@ -23,6 +23,11 @@ l16ui a4, sp, 6
 # CHECK: encoding: [0x52,0x21,0x02]
 l32i a5, sp, 8
 
+# Instruction format RRRN
+# CHECK-INST: l32i.n a5, a1, 8
+# CHECK: encoding: [0x58,0x21]
+l32i.n a5, sp, 8
+
 # CHECK-INST: l32r a2, .LCPI0_0
 # CHECK: encoding: [0x21,A,A]
 # CHECK-FIXUP: fixup A - offset: 0, value: .LCPI0_0, kind: fixup_xtensa_l32rtarget16
@@ -42,3 +47,8 @@ s16i a3, sp, 4
 # CHECK-INST: s32i a5, a1, 8
 # CHECK: encoding: [0x52,0x61,0x02]
 s32i a5, sp, 8
+
+# Instruction format RRI8
+# CHECK-INST: s32i.n a5, a1, 8
+# CHECK: encoding: [0x59,0x21]
+s32i.n a5, sp, 8
