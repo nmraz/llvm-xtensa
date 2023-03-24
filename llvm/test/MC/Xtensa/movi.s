416e96b35ffe49334eacabfef6e34529a63a1188
Noam Raz <noamraz8@gmail.com>
2023-03-24 13:33:56 +0300

    Update xtensa assembler and disassembler tests

--- llvm/test/MC/Xtensa/movi.s
+++ llvm/test/MC/Xtensa/movi.s
@@ -19,3 +19,28 @@ movi a7, 5
 # CHECK-INST: movi.n a2, 3
 # CHECK: encoding: [0x0c,0x32]
 movi.n a2, 3
+
+# Instruction format RI7
+# CHECK-INST: movi.n a2, 64
+# CHECK: encoding: [0x4c,0x02]
+movi.n a2, 64
+
+# Instruction format RI7
+# CHECK-INST: movi.n a2, 85
+# CHECK: encoding: [0x5c,0x52]
+movi.n a2, 85
+
+# Instruction format RI7
+# CHECK-INST: movi.n a2, 95
+# CHECK: encoding: [0x5c,0xf2]
+movi.n a2, 95
+
+# Instruction format RI7
+# CHECK-INST: movi.n a2, -3
+# CHECK: encoding: [0x7c,0xd2]
+movi.n a2, -3
+
+# Instruction format RI7
+# CHECK-INST: movi.n a2, -32
+# CHECK: encoding: [0x6c,0x02]
+movi.n a2, -32
