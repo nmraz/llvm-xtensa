711c89f89c20184df7d79b25a3c9447cbffc97aa
Noam Raz <noamraz8@gmail.com>
2023-04-07 18:36:14 +0300

    Add assembler support for xtensa min/max

--- llvm/test/MC/Xtensa/misc.s
+++ llvm/test/MC/Xtensa/misc.s
@@ -9,3 +9,23 @@ sext a2, a3, 7
 # CHECK-INST: sext a2, a3, 15
 # CHECK: encoding: [0x80,0x23,0x23]
 sext a2, a3, 15
+
+# Instruction format RRR
+# CHECK-INST: min a4, a5, a6
+# CHECK: encoding: [0x60,0x45,0x43]
+min a4, a5, a6
+
+# Instruction format RRR
+# CHECK-INST: minu a4, a5, a6
+# CHECK: encoding: [0x60,0x45,0x63]
+minu a4, a5, a6
+
+# Instruction format RRR
+# CHECK-INST: max a4, a5, a6
+# CHECK: encoding: [0x60,0x45,0x53]
+max a4, a5, a6
+
+# Instruction format RRR
+# CHECK-INST: maxu a4, a5, a6
+# CHECK: encoding: [0x60,0x45,0x73]
+maxu a4, a5, a6
